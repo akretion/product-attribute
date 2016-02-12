0b42fe2011d2c1f87373cd934b02efff4a537e28
Benoît <benoit.guillot@akretion.com.br>
2016-02-12 12:04:12 +0100

    [FIX] wrong branch merged change model from product_product to template

--- product_custom_attributes/models/product.py
+++ product_custom_attributes/models/product.py
@@ -28,10 +28,6 @@ class ProductTemplate(models.Model):
 
     attribute_set_id = fields.Many2one('attribute.set', 'Attribute Set')
 
-
-class ProductProduct(models.Model):
-    _inherit = "product.product"
-
     attribute_group_ids = fields.One2many(
         comodel_name='attribute.group',
         inverse_name='attribute_set_id',
@@ -45,14 +41,14 @@ class ProductProduct(models.Model):
         if not vals.get('attribute_set_id') and vals.get('categ_id'):
             category = self.env['product.category'].browse(vals['categ_id'])
             vals['attribute_set_id'] = category.attribute_set_id.id
-        return super(ProductProduct, self).create(vals)
+        return super(ProductTemplate, self).create(vals)
 
     @api.multi
     def write(self, vals):
         if not vals.get('attribute_set_id') and vals.get('categ_id'):
             category = self.env['product.category'].browse(vals['categ_id'])
             vals['attribute_set_id'] = category.attribute_set_id.id
-        super(ProductProduct, self).write(vals)
+        super(ProductTemplate, self).write(vals)
         return True
 
     @api.multi
@@ -86,7 +82,7 @@ class ProductProduct(models.Model):
                         submenu=False):
         context = self.env.context
 
-        result = super(ProductProduct, self).fields_view_get(
+        result = super(ProductTemplate, self).fields_view_get(
             view_id=view_id, view_type=view_type, toolbar=toolbar,
             submenu=submenu)
 
