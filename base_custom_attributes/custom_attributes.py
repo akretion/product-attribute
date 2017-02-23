9fed43172565c2a3e3c6fc49f2833176e637a5af
Florian da Costa <florian.dacosta@akretion.com>
2017-02-23 19:40:28 +0100

    Allow to create an attribute set without attributes

--- base_custom_attributes/custom_attributes.py
+++ base_custom_attributes/custom_attributes.py
@@ -352,6 +352,8 @@ class attribute_group(orm.Model):
     }
 
     def create(self, cr, uid, vals, context=None):
+        if 'attribute_ids' not in vals:
+            return super(attribute_group, self).create(cr, uid, vals, context)
         for attribute in vals['attribute_ids']:
             if vals.get('attribute_set_id') and attribute[2] and \
                 not attribute[2].get('attribute_set_id'):
