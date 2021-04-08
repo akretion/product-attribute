4d1e7135b146adc52a5486d7646fcd30719dc902
Sébastien BEAU <sebastien.beau@akretion.com>
2021-04-08 14:26:48 +0200

    [FIX] fix migration

--- product_stock_state/models/product_product.py
+++ product_stock_state/models/product_product.py
@@ -9,6 +9,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import api, fields, models
+from odoo.tools.float_utils import float_compare
 
 
 class ProductProduct(models.Model):
@@ -36,6 +37,25 @@ class ProductProduct(models.Model):
         """
         self.ensure_one()
         return self.qty_available
+
+    def _stock_state_check_in_stock(self, qty, precision):
+        return (
+            float_compare(
+                qty,
+                self._get_stock_state_threshold(),
+                precision_digits=precision,
+            )
+            == 1
+        )
+
+    def _stock_state_check_in_limited_stock(self, qty, precision):
+        return float_compare(qty, 0, precision_digits=precision) == 1
+
+    def _stock_state_check_resupplying(self, qty, precision):
+        return float_compare(self.incoming_qty, 0, precision_digits=precision) == 1
+
+    def _stock_state_check_out_of_stock(self, qty, precision):
+        return True
 
     def _available_states(self):
         return [x[0] for x in self._selection_stock_state()]
