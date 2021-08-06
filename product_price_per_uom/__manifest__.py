d94f801ab8561811542d4c356859d5733db0aa4d
hparfr <raphael.reverdy@akretion.com>
2021-08-06 12:05:25 +0200

    product_price_per_uom
    To calculate price per kg

---
+++ product_price_per_uom/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Product Price Per UoM",
+    "summary": "Calculate a price for a given UoM",
+    "version": "12.0.1.0.0",
+    "category": "E-commerce",
+    "website": "www.akretion.com",
+    "author": " Akretion",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "product",
+        "product_logistics_uom",
+    ],
+    "data": ["views/product.xml"],
+}
