54a3981a7d4d3cfbd2fa5b703f6c7164ee430870
mathieu_mpv <mpv.phd@gmail.com>
2025-02-19 15:55:23 +0100

    [UPD] on_demand tests

--- product_stock_state/tests/test_product_stock_state.py
+++ product_stock_state/tests/test_product_stock_state.py
@@ -53,3 +53,28 @@ class TestProductStockState(SavepointCase):
     def test_05_state_out_of_stock(self):
         """Test Stock State computation"""
         self.assertEqual(self.product_threshold_on_product.stock_state, "out_of_stock")
+
+    # on_demand tests
+
+    def test_06_state_on_demand_default_value(self):
+        """Test default value of on_demand"""
+        self.assertFalse(self.product_threshold_on_product.on_demand)
+
+    def test_07_on_demand_inherit(self):
+        """Test on_demand Setting (Setting on a product unique template)"""
+        self.assertEqual(
+            self.product_threshold_on_product._stock_state_check_on_demand(),
+            self.product_threshold_on_product.on_demand,
+        )
+
+    def test_08_state_on_demand_computation(self):
+        """Test on_demand = True Stock State computation 
+        stock_state is set to 'on_demand' regardless of the previous state"""
+        
+        self.product_threshold_on_product.on_demand = True
+        states = self.product_threshold_on_product._available_states()
+
+        for state in states:
+            self.product_threshold_on_product.stock_state = state
+            self.product_threshold_on_product._compute_stock_state()
+            self.assertEqual(self.product_threshold_on_product.stock_state, 'on_demand')
