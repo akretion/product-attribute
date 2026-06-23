c0c380fe05896eee933466aabdd84290e7039e29
Benoît <benoit.guillot@akretion.com.br>
2026-06-23 10:14:06 +0200

    [MIG] product_video_link: Migration to 18.0

--- product_video_link/__manifest__.py
+++ product_video_link/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Product Video",
     "summary": "Link Video on product and category",
-    "version": "14.0.1.0.2",
+    "version": "18.0.1.0.0",
     "category": "Product",
     "development_status": "Alpha",
     "website": "https://github.com/OCA/product-attribute",
